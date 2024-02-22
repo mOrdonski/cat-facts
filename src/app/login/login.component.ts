6ac44a0d5640791fe510eccff7656e2013cdb72c
Maciej Ordoński <maciejordonski@gmail.com>
2024-02-22 17:38:38 +0100

    feat(login): component, service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,62 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+
+import { User } from '../shared/interfaces/user.interface';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatError,
+  ],
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+
+  hidePassword: WritableSignal<boolean> = signal(true);
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated = false;
+  }
+
+  logIn({ username, password }: User): void {
+    const isAuthenticated: boolean = this.authService.authenticate(
+      username,
+      password
+    );
+
+    if (!isAuthenticated) {
+      this.form.setErrors({ invalidCredentials: true });
+    }
+
+    this.router.navigate(['/facts']);
+  }
+}
